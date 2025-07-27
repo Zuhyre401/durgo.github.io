3182da7ca0c2d83718ced19869364d8446391c0d
Zuhyre401 <zuhyreshehjad123@gmail.com>
2025-07-28 00:57:51 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const aboutText = document.getElementById('about-text');
-const readMoreBtn = document.getElementById('read-more-btn');
-
-aboutText.classList.add('collapsed');
-
-readMoreBtn.addEventListener('click', () => {
-    aboutText.classList.toggle('collapsed');
-    readMoreBtn.textContent = aboutText.classList.contains('collapsed') ? 'Read More' : 'Read Less';
-});
